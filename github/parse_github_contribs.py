a8caed937acf7aacb0348eb4015371252d32274d
Russ Savage <russ@influxdata.com>
2020-03-12 23:11:05 -0700

    fix(github): add influx_line_protocol lib

--- github/parse_github_contribs.py
+++ github/parse_github_contribs.py
@@ -6,6 +6,7 @@ import datetime
 import argparse
 import validators
 import json
+from influx_line_protocol import Metric
 
 def main(repo, measurement):
     f = requests.get("https://api.github.com/repos/{}/stats/contributors".format(repo))
@@ -15,9 +16,17 @@ def main(repo, measurement):
     for record in f.json():
         author = record.get("author","")
         for week in record.get("weeks",[]):
-            print("{},author={},org={},repo={} ".format(measurement,author.get("login",""),repo.split("/")[0],repo.split("/")[1]) +
-                  "additions={}i,deletions={}i,commits={}i".format(week.get("a"),week.get("d"),week.get("c")) +
-                  " {}".format(str(week.get("w"))+"000000000"))
+            metric = Metric(measurement)
+            metric.add_tag("author",author.get("login",""))
+            metric.add_tag("org",repo.split("/")[0])
+            metric.add_tag("repo",repo.split("/")[1])
+
+            metric.add_value("additions",int(week.get("a")))
+            metric.add_value("deletions",int(week.get("d")))
+            metric.add_value("commits",int(week.get("c")))
+
+            metric.with_timestamp(week.get("w") * 1000000000)
+            print(metric)
 
 def parseArguments():
     parser = argparse.ArgumentParser()
